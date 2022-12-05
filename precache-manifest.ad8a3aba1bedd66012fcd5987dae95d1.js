d827b41c71281bd6c5f498480ad4e178b980d625
x <crlarrea@outlook.com>
2022-12-05 16:51:21 +0000

    Updates

---
+++ precache-manifest.ad8a3aba1bedd66012fcd5987dae95d1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "56b6aed3ac3911c2c83f",
+    "url": "/honeyman_project/static/css/main.15f55fe2.chunk.css"
+  },
+  {
+    "revision": "56b6aed3ac3911c2c83f",
+    "url": "/honeyman_project/static/js/main.74a04815.chunk.js"
+  },
+  {
+    "revision": "a847f3783fd84b58e5da",
+    "url": "/honeyman_project/static/js/runtime~main.8e368d7b.js"
+  },
+  {
+    "revision": "777b40f904dfe88f79de",
+    "url": "/honeyman_project/static/js/2.f87f4a13.chunk.js"
+  },
+  {
+    "revision": "63977dbad1059e32c49307bedf690343",
+    "url": "/honeyman_project/static/media/aidan.63977dba.png"
+  },
+  {
+    "revision": "cf32b194ab8986af4f362093801c264a",
+    "url": "/honeyman_project/static/media/contract.cf32b194.svg"
+  },
+  {
+    "revision": "ff9caf8db8ef5459022d7402d796289d",
+    "url": "/honeyman_project/static/media/business.ff9caf8d.svg"
+  },
+  {
+    "revision": "c5324580747a05abdf677de2c2837197",
+    "url": "/honeyman_project/static/media/marketing.c5324580.svg"
+  },
+  {
+    "revision": "0134dd8fe6fe708de73909a71d842780",
+    "url": "/honeyman_project/static/media/RobotoCondensed-Regular.0134dd8f.ttf"
+  },
+  {
+    "revision": "905c2728284a6b881d4e417b80f1e79d",
+    "url": "/honeyman_project/static/media/RobotoCondensed-Light.905c2728.ttf"
+  },
+  {
+    "revision": "f66448ad91d21f1ef532cfe6a89c3fdd",
+    "url": "/honeyman_project/static/media/hero_background.f66448ad.svg"
+  },
+  {
+    "revision": "e38804ae070b58fbf4fdd88fd6853929",
+    "url": "/honeyman_project/static/media/RobotoCondensed-Bold.e38804ae.ttf"
+  },
+  {
+    "revision": "5f2e25c1a173ad183ef2ce0323a25990",
+    "url": "/honeyman_project/static/media/about_background.5f2e25c1.svg"
+  },
+  {
+    "revision": "83a4a5ec1af58a49319202f119c0bec5",
+    "url": "/honeyman_project/index.html"
+  }
+];
